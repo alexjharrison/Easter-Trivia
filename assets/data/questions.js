97abd7374fc16db52fd3c44726ceaa93be490f9e
Alex Harrison <alexjharrison@gmail.com>
2020-03-30 17:01:32 -0400

    added questinos

---
+++ assets/data/questions.js
@@ -0,0 +1,109 @@
+export default [
+  {
+    category: `A Vetrinarian's Veteran`,
+    question: `What type of creature was the last known survivor of the Crimean War, who died in 2004?`,
+    answer: `Tortoise`,
+    qpicture: `pciture.jpg`
+  },
+  {
+    category: `Architecture`,
+    question: `What structure did the Chrysler building surpass in 1929 as the tallest building in the world?`,
+    answer: `The Eiffel Tower`
+  },
+  {
+    category: `Food`,
+    question: `What is the name of the clarified butter used in Indian cuisine?`,
+    answer: `Ghee`
+  },
+  {
+    category: ``,
+    question: `Who traveled to North Korea in 2009 to secure the release of two American journalists?`,
+    answer: `Bill Clinton`
+  },
+  {
+    category: ``,
+    question: `What kitchen appliance helped Nike cofounder Bill Bowerman revolutionize running shoes?`,
+    answer: `A waffle iron`
+  },
+  {
+    category: ``,
+    question: `Who performed at the first concerts held at each of the two NY Mets stadiums, one 44 years after the other?`,
+    answer: `Paul McCartney`
+  },
+  {
+    category: ``,
+    question: `What are you afraid of if you suffer from pterygophobia?`,
+    answer: `Flying`
+  },
+  {
+    category: `The One Percent`,
+    question: `Astronomer Tycho Brahe is estimated to have owned 1% of all the money in Denmark. This was enough to buy him a silver nose prosthetic, but no to save his pet from a tragic alcohol-fueled fall down the stairs.  What type of animal was this pet?`,
+    answer: `A Moose`
+  },
+  {
+    category: ``,
+    question: `What German car company name means "people's car"?`,
+    answer: `Volkswagen`
+  },
+  {
+    category: `Ready Player One`,
+    question: `What classic arcade game does Seinfeld's George buy from a pizza parlor to preserve his high score?`,
+    answer: `Frogger`
+  },
+  {
+    category: ``,
+    question: `What classic children's toy started out as wallpaper cleaner?`,
+    answer: `Play-doh`
+  },
+  {
+    category: ``,
+    question: `What singer renamed Silver Dollar City, Tennessee, after herself?`,
+    answer: `Dolly Parton`
+  },
+  {
+    category: ``,
+    question: `In Greek Mythology, what nymph was jilted by Narcissus, then pined away for him until nothing but her voice remained?`,
+    answer: `Echo`
+  },
+  {
+    category: ``,
+    question: `What were president Lyndon Johnson's pets Soup and Noodles - peacocks, goats, or donkeys?`,
+    answer: `Donkeys`
+  },
+  {
+    category: ``,
+    question: `What city pretended to secede from the US in 1982, declaring itself the Conch Republic?`,
+    answer: `Key West`
+  },
+  {
+    category: ``,
+    question: `What cylindrical food container was so prized by its inventor that he requested his ashes be interred in one?`,
+    answer: `The Pringles can`
+  },
+  {
+    category: ``,
+    question: `Fill in the blank in this phrase commonly used in design mockups - "Lorem _____ dolor sit amet."`,
+    answer: ``
+  },
+  {
+    category: ``,
+    question: `Which city is at a latitude most similar to Tokyo - Denver, Houston, Memphis, or Seattle?`,
+    answer: `Memphis`
+  },
+  {
+    category: ``,
+    question: `What last name did Melinda French take in 1994 when she wed her company's CEO?`,
+    answer: `Gates`
+  },
+  { category: ``, question: ``, answer: `` },
+  { category: ``, question: ``, answer: `` },
+  { category: ``, question: ``, answer: `` },
+  { category: ``, question: ``, answer: `` },
+  { category: ``, question: ``, answer: `` },
+  { category: ``, question: ``, answer: `` },
+  { category: ``, question: ``, answer: `` },
+  { category: ``, question: ``, answer: `` },
+  { category: ``, question: ``, answer: `` },
+  { category: ``, question: ``, answer: `` },
+  { category: ``, question: ``, answer: `` }
+]
