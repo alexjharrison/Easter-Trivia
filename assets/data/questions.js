1efaad489da8d13135b89507b750b027847af0a6
Alex Harrison <alexjharrison@gmail.com>
2020-04-10 17:21:08 -0400

    first set of pictures

--- assets/data/questions.js
+++ assets/data/questions.js
@@ -1,69 +1,86 @@
+import { BIconTypeBold, BIconArrow90degDown } from 'bootstrap-vue'
+
 export default [
   {
     category: `Food`,
     question: `What is the name of the clarified butter used in Indian cuisine?`,
     answers: [`Ghee`],
-    points: 1
+    points: 1,
+    qpic: `Q1.jpg`,
+    apic: `A1.jpg`
   },
   {
     category: `TV`,
     question: `What classic arcade game does Seinfeld's George buy from a pizza parlor to preserve his high score?`,
     answers: [`Frogger`],
-    points: 2
+    points: 2,
+    qpic: `Q2.jpg`,
+    apic: `A2.gif`
   },
   {
     category: `Crime`,
     question: `Did Carole Baskin kill her first husband Don Lewis and feed him to her tigers?`,
     answers: [`¯\\_(ツ)_/¯`],
-    points: 3
+    points: 3,
+    qpic: `Q3.png`,
+    apic: `A3.gif`
   },
   {
     category: `Architecture`,
     question: `What structure did the Chrysler building surpass in 1929 as the tallest building in the world?`,
     answers: [`The Eiffel Tower`],
-    points: 4
-  },
-  {
-    category: `Food`,
-    question: `What is the name of the clarified butter used in Indian cuisine?`,
-    answers: [`Ghee`],
-    points: 5
+    points: 4,
+    qpic: `Q4.jpg`,
+    apic: `A4.jpg`
   },
   {
     category: `Math`,
     question: `Factor the quadratic equation x2 + 3x - 4 into the format (x + ?)(x - ?)`,
     answers: [`(x + 4)(x - 1)`],
-    pickAPoint: true
+    points: 5,
+    qpic: `tbd.jpg`,
+    apic: `tbd.jpg`
   },
+
   {
     category: `Literature`,
     question: `What well-known author of children’s books partially credited his friendship with JRR Tolkien for inspiring his work in theology?`,
     answers: [`C.S. Lewis`],
-    pickAPoint: true
+    pickAPoint: true,
+    qpic: `Q6.jpg`,
+    apic: `A6.jpg`
   },
   {
     category: `Music`,
     question: `Who performed at both of the first concerts held at the two NY Mets stadiums, one 44 years after the other?`,
     answers: [`Paul McCartney`],
-    pickAPoint: true
+    pickAPoint: true,
+    qpic: `Q7.jpg`,
+    apic: `A7.jpg`
   },
   {
     category: `Mythology`,
     question: `In Greek Mythology, what nymph was jilted by Narcissus, then pined away for him until nothing but her voice remained?`,
     answers: [`Echo`],
-    pickAPoint: true
+    pickAPoint: true,
+    qpic: `tbd.jpg`,
+    apic: `tbd.jpg`
   },
   {
     category: `Technology`,
     question: `What company based in Murray Hill, NJ is credited with inventing the first working transistor, the basic building block of all modern computing chips?`,
     answers: [`Bell Labs`],
-    pickAPoint: true
+    pickAPoint: true,
+    qpic: `Q9.jpg`,
+    apic: `BIconArrow90degDown.jpg`
   },
   {
     category: `Elements`,
     question: `What South American country’s name comes from the latin word for silver?`,
     answers: [`Argentina`],
-    pickAPoint: true
+    pickAPoint: true,
+    qpic: `Q10.jpg`,
+    apic: `tbd.jpg`
   },
   {
     category: `Vocabulary`,
@@ -114,7 +131,8 @@ export default [
       `Umbrage`,
       `Underage`,
       `Vicarage`
-    ]
+    ],
+    qpic: `Q11.gif`
   },
   {
     category: `History`,
@@ -122,13 +140,17 @@ export default [
     q1: `What was the political movement that grew out of the principles he used to govern that city?`,
     q2: `What is the name of the Italian politician most associated with that movement?`,
     answers: [`Fascism`, `Benito Amilcare Andrea Mussolini`],
-    multi: 2
+    multi: 2,
+    qpic: ` tbd.jpg`,
+    apic: ` tbd.jpg`
   },
   {
     category: `Astronomy`,
     question: `The two moons of Mars are named after the mythical twin sons of Ares, whose names translate to Panic and Terror.  What are the names of the moons?`,
     answers: [`Phobos`, `Deimos`],
-    multi2: true
+    multi2: true,
+    qpic: ` tbd.jpg`,
+    apic: ` tbd.jpg`
   },
   {
     category: `Music`,
@@ -145,48 +167,64 @@ export default [
       `Mary`,
       `Jessica`
     ],
-    multi2: true
+    multi2: true,
+    qpic: ` tbd.jpg`,
+    apic: ` tbd.jpg`
   },
   {
     category: `Geography`,
     question: `Which three East African countries border Lake Victoria?`,
     answers: [`Uganda`, `Kenya`, `Tanzania`],
-    multi3: true
+    multi3: true,
+    qpic: ` tbd.jpg`,
+    apic: ` tbd.jpg`
   },
   {
     category: `Presidents`,
     question: `All US presidents elected between 1800 to 1850 were members of one of 3 political parties.  What were those parties?`,
     answers: [`Democratic`, `Whig`, `Democratic-Republican`],
-    multi3: true
+    multi3: true,
+    qpic: ` tbd.jpg`,
+    apic: ` tbd.jpg`
   },
   {
     category: `Etymology`,
     question: `Which word from the Italian phrase for “forty days” originated during the 14th century to describe a method for dealing with the plague?`,
     answers: [`Quarantine`],
-    wager: 3
+    wager: 3,
+    qpic: ` tbd.jpg`,
+    apic: ` tbd.jpg`
   },
   {
     category: `Politically Incorrect`,
     question: `After 33 years without a Major League Baseball team, what city had to find a new name for their new team in 2005 because the name of their previous team had become too controversial?`,
     answers: [`Washington DC (Senators => Nationals)`],
-    wager: 5
+    wager: 5,
+    qpic: ` tbd.jpg`,
+    apic: ` tbd.jpg`
   },
   {
     category: `Business`,
     question: `What kitchen appliance helped Nike co-founder Bill Bowerman revolutionize running shoes?`,
     answers: [`A waffle iron`],
-    wager: 10
+    wager: 10,
+    qpic: ` tbd.jpg`,
+    apic: ` tbd.jpg`
   },
   {
     category: `The 1%`,
     question: `Astronomer Tycho Brahe is estimated to have owned 1% of all the money in Denmark. This was enough to buy him a silver nose prosthetic, but not to save his pet from a tragic alcohol-fueled fall down the stairs.  What type of animal was this pet?`,
     answers: [`A Moose`],
-    wager: 15
+    wager: 15,
+    qpic: ` tbd.jpg`,
+    apic: ` tbd.jpg`
   },
   {
     category: `Actors`,
     question: `Which English actor, born Maurice Joseph Micklewhite Jr. in Rotherhithe, London, is one of two actors to be nominated for an Academy Award in every decade from the 60’s until the 2000’s`,
     answers: [`Michael Caine`],
-    wager: 25
+    wager: 25,
+    qpic: ` tbd.jpg`,
+    apic: ` tbd.jpg`
   }
 ]
